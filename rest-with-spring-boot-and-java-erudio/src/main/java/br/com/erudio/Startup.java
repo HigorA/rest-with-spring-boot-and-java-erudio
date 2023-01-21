84d07d142ba7f55e1f10b04ce256b9cd3b491cca
Higor <higor.a.santos12@hotmail.com>
2023-01-21 20:29:12 -0300

    Seção 5

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
@@ -2,9 +2,11 @@ package br.com.erudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Startup {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
